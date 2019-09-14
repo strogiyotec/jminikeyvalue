a4024d29a85473b5d3c80d81a4775aaf1f9e8e61
strogiyotec <almas337519@gmail.com>
2019-09-14 19:01:10 +0600

    base structure

---
+++ src/main/java/com/lightbox/jmkv/Main.java
@@ -0,0 +1,24 @@
+package com.lightbox.jmkv;
+
+/**
+ * Main class.
+ */
+public final class Main {
+
+    /**
+     * Ctor.
+     */
+    private Main() {
+
+    }
+
+    /**
+     * Entry point.
+     *
+     * @param args Command line argc
+     */
+    public static void main(final String[] args) {
+
+    }
+}
+
---
+++ src/main/java/com/lightbox/jmkv/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Main package for jmkv.
+ */
+package com.lightbox.jmkv;
